16c791150ffc6db5c5b408fce9adfbd64efa0365
Theo Fabien <73390220+TheoFABIEN@users.noreply.github.com>
2023-04-28 10:34:55 +0200

    Update Coherent_point_drift.py

--- Image registration/Coherent_point_drift.py
+++ Image registration/Coherent_point_drift.py
@@ -34,8 +34,8 @@ def CPD(img1_o,img2_o,Multimodal=False,Multistart=False,Sym=False,Param=False,bo
             arimg2 = np.array(img2)
             arimg2[arimg2 > 40] = 255
             arimg2[arimg2 <= 40] = 0
-            arimg2 = morphology.remove_small_holes(arimg2.astype(int),5000)    
-            arimg2 = morphology.remove_small_objects(arimg2.astype(bool),5000)   
+            arimg2 = morphology.remove_small_holes(arimg2.astype(int), 5000)    
+            arimg2 = morphology.remove_small_objects(arimg2.astype(bool), 5000)   
             img2[arimg2 == False] = 0
 
         #Zoom on the leaf in image 2:
@@ -43,15 +43,24 @@ def CPD(img1_o,img2_o,Multimodal=False,Multistart=False,Sym=False,Param=False,bo
         if len(loc[0]) == 0 or len(loc[1]) == 0:
           print('Error ! No pixel is detected in this image !')
           return None
-        width = np.array([min(loc[0]),img2.shape[0]-max(loc[0]),
-                          min(loc[1]),img2.shape[1]-max(loc[1])])
+        width = np.array([min(loc[0]), img2.shape[0]-max(loc[0]),
+                          min(loc[1]), img2.shape[1]-max(loc[1])])
         for w in width:     # Correct eventual negative values
             w = max(w, 0)
 
-        img2_crop = ski.util.crop(img2,((width[0],width[1]),
-                                        (width[2],width[3])),copy=True)
+        img2_crop = ski.util.crop(
+            img2,
+            ((width[0],width[1]),(width[2],width[3])),
+            copy = True
+        )
         scale = np.max(img1.shape[0:2]) / np.max(img2_crop.shape[0:2])
-        img2 = cv2.resize(img2_crop,(0,0),fx=scale,fy=scale)
+        
+        img2 = cv2.resize(
+            img2_crop,
+            (0,0),
+            fx = scale,
+            fy = scale
+        )
 
     # Extract masks
     img1_m = ski.color.rgb2gray(img1) < 1 
@@ -68,7 +77,7 @@ def CPD(img1_o,img2_o,Multimodal=False,Multistart=False,Sym=False,Param=False,bo
         print('Iteration: {:d}\nError: {:06.4f}'.format(iteration, error))
     callback = partial(trace) 
 
-    background = 0 if Multimodal else [255,255,255]
+    background = 0 if Multimodal else [255, 255, 255]
     Multistart = True if Sym else Multistart 
 
     if Multistart:
@@ -79,7 +88,7 @@ def CPD(img1_o,img2_o,Multimodal=False,Multistart=False,Sym=False,Param=False,bo
        (ski.transform.rotate(img2,270,resize=True)*h).astype('uint8')]
 
        y1, x1, z1 = img1_o.shape
-       img1 = 255 * np.ones((border*2+y1, border*2+x1, z1), dtype=np.uint8)
+       img1 = 255 * np.ones((border*2+y1, border*2+x1, z1), dtype = np.uint8)
        img1[border:border+y1, border:border+x1] = img1_o
        img1_m = ski.color.rgb2gray(img1) <1 
        img1_c = ski.feature.canny(img1_m) 
@@ -100,7 +109,13 @@ def CPD(img1_o,img2_o,Multimodal=False,Multistart=False,Sym=False,Param=False,bo
            reg.register(callback)
            # Affine transformation with OpenCV
            T = np.float32(np.c_[reg.R, [reg.t[1],reg.t[0]]]*reg.s) # Transformation matrix
-           s_new = cv2.warpAffine(s, T, dsize = (img1.shape[1],img1.shape[0]), borderValue = background)
+           s_new = cv2.warpAffine(
+               s, 
+               T, 
+               dsize = (img1.shape[1],img1.shape[0]), 
+               borderValue = background
+           )
+            
            if Multimodal:
                s_c = ski.feature.canny(s_new > 0) # Extract contour with Canny filter
            else :
@@ -108,6 +123,7 @@ def CPD(img1_o,img2_o,Multimodal=False,Multistart=False,Sym=False,Param=False,bo
 
            s_p = np.transpose(np.where(s_c)) 
            dst = np.mean(ssd.cdist(img1_p,s_p,'seuclidean'))
+            
            similarity.append(-dst), Tm.append((reg.R,reg.t,reg.s)), new.append(s_new)
 
        print('Elapsed time: ' + str(time.time()-t0) + ' s')
@@ -123,7 +139,12 @@ def CPD(img1_o,img2_o,Multimodal=False,Multistart=False,Sym=False,Param=False,bo
     reg = RigidRegistration(**{ 'X': img1_p, 'Y':img2_p,'max_iterations':200, 'tolerance':0.0001})
     reg.register(callback)
     T = np.float32(np.c_[reg.R,[reg.t[1],reg.t[0]]]*reg.s)
-    img2_new = cv2.warpAffine(img2, T, dsize = (img1.shape[1],img1.shape[0]), borderValue = background)
+    img2_new = cv2.warpAffine(
+        img2, 
+        T, 
+        dsize = (img1.shape[1],img1.shape[0]), 
+        borderValue = background
+    )
 
     print('Elapsed time: ' + str(time.time()-t0) + ' s')
     if Param:
